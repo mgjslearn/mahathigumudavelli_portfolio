b0a10fe4746f4aeab9c76a2a2b4d51e3ab5a567a
Mahathi Gumudavelli <101474281+mgjslearn@users.noreply.github.com>
2025-02-04 00:10:48 -0500

    Delete src directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Arts from './Arts';
-import Contact from './Contact';
-
-
-export { Home, About, Projects, Contact, Arts };
