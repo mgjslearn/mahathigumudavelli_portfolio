6c4a27d89f679c7ed224b7eb37ecdf06f910c6e6
Mahathi Gumudavelli <101474281+mgjslearn@users.noreply.github.com>
2025-02-18 19:28:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element= {<Home />} />
-                    <Route path="/about" element= {<About />} />
+                    <Route path="/mahathigumudavelli_portfolio/" element= {<Home />} />
+                    <Route path="/mahathigumudavelli_portfolio/about" element= {<About />} />
                     <Route path="/mahathigumudavelli_portfolio/projects" element= {<Projects />} />
                     <Route path="/mahathigumudavelli_portfolio/contact" element= {<Contact />} />
                     <Route path="/arts" element= {<Arts />} />
