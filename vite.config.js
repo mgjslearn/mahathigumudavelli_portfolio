01e50cca1f8bbdd73f2d11514eb3099aed782c83
mgjslearn <mahag.student@gmail.com>
2025-02-26 11:43:46 -0500

    vite config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/mahathigumudavelli_portfolio/", 
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: '/mahathigumudavelli_portfolio/', 
 })
